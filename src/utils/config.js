133f6c75c16fa937d671cef056d44f3c3b6b56da
Andrea Delle Chiaie <andrea@dellechiaie.it>
2019-09-04 17:20:27 +0200

    Initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,8 @@
+const MapBoxAccessToken = 'pk.eyJ1IjoibWlzdGVyZGVsbGUiLCJhIjoiY2swNDI4MDJ1MmtzYzNtcGtvNXhxYTN1dCJ9.Bww8ALxjmG20_fCtakgWYw'
+
+const DarkSkyAccessToken = '34241708cd871f2de3e0f0097de38f4e'
+
+module.exports = {
+    MapBoxAccessToken,
+    DarkSkyAccessToken
+}
